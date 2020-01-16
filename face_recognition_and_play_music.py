56afe0a509a59579031e763d83abedc4c24ff6cd
Scott. J <41283107+smiletoeveryone@users.noreply.github.com>
2020-01-16 17:06:33 +0800

    Add files via upload

---
+++ face_recognition_and_play_music.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import face_recognition
+import cv2
+import numpy as np
+from playsound import playsound
+
+
+
+# Get a webcam port (the default one should be the #0)
+video_capture = cv2.VideoCapture(4) 
+
+# Load a picture for training it.
+obama_image = face_recognition.load_image_file("obama.jpg")
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+
+# Create arrays of known face encodings
+known_face_encodings = [
+    obama_face_encoding,
+   
+]
+
+# the name would display on the picture
+known_face_names = [
+     
+    "This is Obama"
+]
+
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read(4)
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_frame = frame[:, :, ::-1]
+
+    # Find all the faces and face enqcodings in the frame of video
+    face_locations = face_recognition.face_locations(rgb_frame)
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+
+    # Loop through each face in this frame of video
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+
+        name = "Unknown"
+
+        
+        # instead, use the known face with the smallest distance to the new face
+        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        
+for i in range (0, 2):
+    if matches[best_match_index]:
+        playsound('/home/fiftycentsjj/Downloads/programming_practice/python/bluetooth/TwinkleTwinkleLittleStar.mp3')
+            
+
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
+
+'''
+# try to process it next
+if name == known_face_names["Hi, Pikacu is here"]:
+    print('everything is fine')
+'''
