d175ef68aa5cf9de23307a8f31ef15d92b835981
Scott. J <41283107+smiletoeveryone@users.noreply.github.com>
2020-01-16 17:12:20 +0800

    Update face_recognition_and_play_music.py

--- face_recognition_and_play_music.py
+++ face_recognition_and_play_music.py
@@ -52,7 +52,7 @@ while True:
         best_match_index = np.argmin(face_distances)
         if matches[best_match_index]:
             name = known_face_names[best_match_index]
-            playsound('/home/fiftycentsjj/Downloads/programming_practice/python/bluetooth/TwinkleTwinkleLittleStar.mp3')
+            playsound('path of mp3')
 
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
